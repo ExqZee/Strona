5f4a081d75bd72c3ed0727d8aab00b4add546f7f
Freizen77 <105147769+Freizen77@users.noreply.github.com>
2022-05-24 18:57:44 +0200

    Update skrypty.js

--- js/skrypty.js
+++ js/skrypty.js
@@ -1,1 +1,14 @@
 
+
+        function Historia(){
+           window.location.href='index.html' ;
+        }
+        function fast(){
+           window.location.href='najsz.html' ;
+        } 
+        function old(){
+           window.location.href='najs.html' ;
+        }
+        function expensive(){
+           window.location.href='najd.html' ;
+        }
